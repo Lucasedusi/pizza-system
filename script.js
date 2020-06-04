bcc7a686296fc473cf3fce99dc224c1905b4ccfc
Lucasedusi <lucasmto.16@hotmail.com>
2020-06-04 18:09:02 -0300

    Create modal

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const myDocument = (element) => document.querySelector(element);
 const myDocumentAll = (element) => document.querySelectorAll(element);
 
+const cartModal = (e) => {
+  e.preventDefault();
+
+  myDocument('.pizzaWindowArea').style.opacity = 0;
+  myDocument('.pizzaWindowArea').style.display = 'flex';
+  setTimeout(() => {
+    myDocument('.pizzaWindowArea').style.opacity = 1;
+  }, 200);
+};
+
 pizzaJson.map((item) => {
   pizzaItem = myDocument('.models .pizza-item').cloneNode(true);
 
@@ -8,6 +18,8 @@ pizzaJson.map((item) => {
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  pizzaItem.querySelector('a').addEventListener('click', cartModal);
 
   myDocument('.pizza-area').append(pizzaItem);
 });
