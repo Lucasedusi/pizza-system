cf7a6d5399dca1c5c366d62d982c9a5635a61880
Lucasedusi <lucasmto.16@hotmail.com>
2020-06-04 10:40:24 -0300

    listing pizzas

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const MyDocument = (element) => document.querySelector(element);
-const MyDocumentAll = (element) => document.querySelectorAll(element);
+const myDocument = (element) => document.querySelector(element);
+const myDocumentAll = (element) => document.querySelectorAll(element);
 
-pizzaJson.map((item, index) => {
-  pizzaItem = MyDocument('.models .pizza-item').cloneNode(true);
+pizzaJson.map((item) => {
+  pizzaItem = myDocument('.models .pizza-item').cloneNode(true);
 
-  MyDocument('.pizza-area').append(pizzaItem);
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  myDocument('.pizza-area').append(pizzaItem);
 });
